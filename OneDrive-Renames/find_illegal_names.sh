4583c64d7e64db1d11b376aa1fcbb66c430ded5d
yeagerghost <yeagerghost@gmail.com>
2018-01-12 16:00:47 -0500

    Initia commit

--- OneDrive-Renames/find_illegal_names.sh
+++ OneDrive-Renames/find_illegal_names.sh
@@ -2,11 +2,10 @@
 
 # Written on Mac OS
 
-# illegal characters considered for this script are ==> * : < > ? / \ | "
+# illegal characters considered for this script are ==> * : < > ? / \ | " and files that begin with a space, files that end with a .
 illegal_string_regex="\*|:|<|>|\?|\\|/|\||\\\"| +\$|^ +|\.\$"
 path="/Path goes here"
 
 find_command="find \"$path\" | grep -E \"$illegal_string_regex\""
 
-eval $find_command > illegal_dirs
-echo $find_command 
+eval $find_command > illegal_names
